2859693ada0bde348d1a303c5215dfe822a243db
rohit <rhtry.tech@gmail.com>
2023-01-11 00:13:28 +0530

    adding files ...!

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,19 +1,79 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
-import {pageHOC} from "../components";
+import {CustomButton, CustomInput, PageHOC} from "../components";
+import {useGlobalContext} from "../context";
 
 const Home = () => {
-  return <div></div>;
+  // const { contract, walletAddress, gameData, setShowAlert, setErrorMessage } = useGlobalContext();
+  const {contract, walletAddress} = useGlobalContext();
+
+  const [playerName, setPlayerName] = useState("");
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    // try {
+    //   const playerExists = await contract.isPlayer(walletAddress);
+    //   if (!playerExists) {
+    //     await contract.registerPlayer(playerName, playerName, {
+    //       gasLimit: 500000,
+    //     });
+    //     setShowAlert({
+    //       status: true,
+    //       type: "info",
+    //       message: `${playerName} is being summoned!`,
+    //     });
+    //     setTimeout(() => navigate("/create-battle"), 8000);
+    //   }
+    // } catch (error) {
+    //   setErrorMessage(error);
+    // }
+  };
+
+  // useEffect(() => {
+  //   const createPlayerToken = async () => {
+  //     const playerExists = await contract.isPlayer(walletAddress);
+  //     const playerTokenExists = await contract.isPlayerToken(walletAddress);
+
+  //     if (playerExists && playerTokenExists) navigate("/create-battle");
+  //   };
+
+  //   if (contract) createPlayerToken();
+  // }, [contract]);
+
+  // useEffect(() => {
+  //   if (gameData.activeBattle) {
+  //     navigate(`/battle/${gameData.activeBattle.name}`);
+  //   }
+  // }, [gameData]);
+
+  return (
+    walletAddress && (
+      <div className="flex flex-col">
+        <CustomInput
+          label="Name"
+          placeHolder="Enter your player name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
+
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
+    )
+  );
 };
 
-// wrapping home with HOC > pageHOC
-export default pageHOC(
+export default PageHOC(
   Home,
   <>
-    Welcome To Avax Gods <br /> a Web3 NFT Card Game
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
   </>,
   <>
-    Connect your WEB3.0 wallet to start playing <br /> the ultimate Web3 Battle
-    Card Game !
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
   </>
 );
